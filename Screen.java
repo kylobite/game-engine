87a9843866554a8a0cbeb49b70249bf605538149
kylobite <kylolink@gmail.com>
2014-04-01 21:55:10 -0500

    Added tiles

--- Screen.java
+++ Screen.java
@@ -1,14 +1,24 @@
+import java.util.Random;
 
 public class Screen {
 
     private int width, height;
+    private Random random = new Random();
+
+    public static int tileSize = 4;
+    public static int mapSize  = 64;
 
     public int[] pixels;
+    public int[] tiles = new int[mapSize * mapSize];
 
     public Screen(int width, int height) {
         this.width  = width;
         this.height = height;
         this.pixels = new int[width * height];
+
+        for (int i = 0; i < mapSize * mapSize; i++) {
+            tiles[i] = random.nextInt(0xffffff);
+        }
     }
 
     public void clear() {
@@ -18,11 +28,13 @@ public class Screen {
     }
 
     public void render() {
+        int ms = (int)(Math.round(Math.log(mapSize) / Math.log(2)));
         for (int y = 0; y < height; y++) {
             if (y < 0 || y >= height) continue;
             for (int x = 0; x < width; x++) {
                 if (x < 0 || x >= width) continue;
-                pixels[x + y * width] = 0x708090;
+                int tileIndex = (x >> tileSize) + ((y >> tileSize) << ms);
+                pixels[x + y * width] = tiles[tileIndex];
             }
         }
     }
